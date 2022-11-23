aee1bac84b7cb3b96134d53cb86f5e243fc47868
Tomas Izuel <tomasizuel@gmail.com>
2022-11-23 18:42:12 -0300

    te mejore el programa pa dea

--- public/app.js
+++ public/app.js
@@ -1,9 +1,17 @@
 const btn_respuesta = document.getElementById('btn_respuesta');
 const btn_siguiente = document.getElementById('btn_siguiente');
+const btn_siguiente_success = document.getElementById('btn_siguiente_success');
+const btn_siguiente_danger = document.getElementById('btn_siguiente_danger');
+const preguntas = document.getElementById('preguntas');
+const correct = document.getElementById('correct');
+
 
 const pregunta_txt = document.getElementById('pregunta_txt');
 const respuesta = document.getElementById('respuesta');
 const box_respuesta = document.getElementById('box_respuesta');
+
+let preguntasCont = 0;
+let aciertosCont = 0;
 
 let dataMostrada;
 
@@ -24,8 +32,19 @@ btn_respuesta.onclick = () => {
     `; 
 }
 
-btn_siguiente.onclick = () => {
+btn_siguiente_success.onclick = () => {
     respuesta.value=""
+    preguntasCont = preguntasCont + 1;
+    aciertosCont = aciertosCont + 1;
+    preguntas.innerHTML = `${preguntasCont}`
+    correct.innerHTML = `${aciertosCont}`
+    inicializar()   
+}
+
+btn_siguiente_danger.onclick = () => {
+    respuesta.value=""
+    preguntasCont = preguntasCont + 1;
+    preguntas.innerHTML = `${preguntasCont}`
     inicializar()
 }
 
