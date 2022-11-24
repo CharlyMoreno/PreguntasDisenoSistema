8a45d843865d349ed049c98dab02826edb6868a8
Tomas Izuel <tomasizuel@gmail.com>
2022-11-23 23:05:57 -0300

    full facha, no te pienso hacer un commit como la gente

--- public/app.js
+++ public/app.js
@@ -1,5 +1,12 @@
+class Pregunta{
+    constructor(pregunta, respuesta, validez){
+        this.pregunta = pregunta;
+        this.respuesta = respuesta;
+        this.validez = validez;
+    }
+}
+
 const btn_respuesta = document.getElementById('btn_respuesta');
-const btn_siguiente = document.getElementById('btn_siguiente');
 const btn_siguiente_success = document.getElementById('btn_siguiente_success');
 const btn_siguiente_danger = document.getElementById('btn_siguiente_danger');
 const preguntas = document.getElementById('preguntas');
@@ -26,10 +33,12 @@ function inicializar(){
 }
 
 btn_respuesta.onclick = () => {
+    box_respuesta.style.maxHeight = "20rem";
     box_respuesta.innerHTML =`<div class="alert alert-info" role="alert">
         ${dataMostrada.respuesta}
     </div>
     `; 
+    console.log(dataMostrada.respuesta)
 }
 
 btn_siguiente_success.onclick = () => {
@@ -45,6 +54,7 @@ btn_siguiente_danger.onclick = () => {
     respuesta.value=""
     preguntasCont = preguntasCont + 1;
     preguntas.innerHTML = `${preguntasCont}`
+    correct.innerHTML = `${aciertosCont}`
     inicializar()
 }
 
